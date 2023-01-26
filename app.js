d97f4b72fb6a20d75b76c8995f439be80d67e779
Sadiq Shah <52124323+sadiqshah786@users.noreply.github.com>
2023-01-26 07:27:01 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -19,24 +19,4 @@ light.addEventListener("click", () => {
     activeListItem.className = "list-item light active";
 });
 
-// const colors = document.querySelectorAll(".color-box");
-// const sidebar = document.querySelector(".sidebar");
-// const activeListItem = document.querySelector(".list-item.active");
 
-// colors.forEach((color) => {
-//   color.addEventListener("click", () => {
-//     if (color.classList.contains("dark")) {
-//       sidebar.style.backgroundColor = "#111827";
-//       activeListItem.style.backgroundColor = "#374151";
-//     } else if (color.classList.contains("night")) {
-//       sidebar.style.backgroundColor = "#312e81";
-//       activeListItem.style.backgroundColor = "#4338ca";
-//     } else {
-//       sidebar.style.backgroundColor = "#f3f4f6";
-//       activeListItem.style.backgroundColor = "#d1d5db";
-//     }
-//   });
-// });
-
-
-// classList.add remove
